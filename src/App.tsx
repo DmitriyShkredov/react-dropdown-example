62ed63367a60c6201aebd8bbc48c3ecced405d7a
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2024-05-30 13:16:00 +0300

    feat: add groups options

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
-import Dropdown from "react-dropdown";
 import { Post } from "./Post/Post";
+import Dropdown from "react-dropdown";
+import type { Option, Group } from "react-dropdown";
 import "./App.css";
 
-import type { Option } from "react-dropdown";
-
 function App() {
-  const defItem: Option = { value: "", label: "123" };
+  const defItem: Option = { value: "", label: "Выбор категории" };
   const [state, setState] = useState(defItem);
-  const options = [
-    { value: "cat1", label: "Категория 1" },
-    { value: "cat2", label: "Категория 2" },
-    { value: "cat3", label: "Категория 3" },
-    { value: "cat4", label: "Категория 4" },
+  const options: Group[] = [
+    {
+      type: "group",
+      name: "group1",
+      items: [
+        { value: "cat1", label: "Категория 1" },
+        {
+          value: "cat2",
+          label: "Категория 2",
+          className: "Dropdown-option-two",
+        },
+      ],
+    },
+    {
+      type: "group",
+      name: "group2",
+      items: [
+        { value: "cat3", label: "Категория 3" },
+        { value: "cat4", label: "Категория 4" },
+      ],
+    },
   ];
 
   return (
@@ -20,15 +35,12 @@ function App() {
       <header className="header">
         <Dropdown
           options={options}
-          onChange={(item) => setState(item)}
-          value={state}
           placeholder="Выбор категории"
+          value={state}
+          onChange={(item) => setState(item)}
         />
       </header>
       <section className="content">
-        <Post />
-        <Post />
-        <Post />
         <Post />
       </section>
     </>
--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -12,10 +12,13 @@ export const Post: FC = () => {
       <p className="post-text">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam illum
         sunt amet numquam ea eveniet optio sit voluptatibus sapiente enim soluta
-        quia doloremque, voluptatem labore quo! Commodi, quos reprehenderit
-        laudantium voluptas obcaecati quas veniam distinctio, consectetur
-        adipisicing elit. Quam illum sunt amet numquam ea eveniet optio sit
-        voluptatibus sapiente enim soluta quia doloremque, voluptatem labore.
+        quia doloremque, quo! Commodi, quos reprehenderit laudantium voluptas
+        obcaecati quas veniam distinctio, consectetur adipisicing elit. Quam
+        illum sunt amet numquam ea eveniet optio sit voluptatibus sapiente enim
+        soluta quia doloremque, voluptatem labore. Lorem ipsum dolor sit elit.
+        Quis maxime tenetur harum. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Quod necessitatibus ipsum non quisquam aperiam. Lorem
+        ipsum dolor sit, adipisicing elit. Porro nam qui, doloremque dolor!
       </p>
     </div>
   );
