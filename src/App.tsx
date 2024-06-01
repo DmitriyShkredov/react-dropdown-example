531ef8a809a296200ffa3183fd50fed9862d4360
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2024-06-01 12:07:22 +0300

    feat: add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,24 @@
 import { useState } from "react";
 import { Post } from "./Post/Post";
 import Dropdown from "react-dropdown";
-import type { Option, Group } from "react-dropdown";
 import "./App.css";
+// import "react-dropdown/style.css";
+
+import type { Option } from "react-dropdown";
 
 function App() {
-  const defItem: Option = { value: "", label: "Выбор категории" };
-  const [state, setState] = useState(defItem);
-  const options: Group[] = [
-    {
-      type: "group",
-      name: "group1",
-      items: [
-        { value: "cat1", label: "Категория 1" },
-        {
-          value: "cat2",
-          label: "Категория 2",
-          className: "Dropdown-option-two",
-        },
-      ],
-    },
-    {
-      type: "group",
-      name: "group2",
-      items: [
-        { value: "cat3", label: "Категория 3" },
-        { value: "cat4", label: "Категория 4" },
-      ],
-    },
+  const options: Option[] = [
+    { value: "cat1", label: "Категория 1" },
+    { value: "cat2", label: "Категория 2" },
+    { value: "cat3", label: "Категория 3" },
+    { value: "cat4", label: "Категория 4" },
   ];
-
+  const [state, setState] = useState(options[0]);
   return (
     <>
       <header className="header">
         <Dropdown
           options={options}
-          placeholder="Выбор категории"
           value={state}
           onChange={(item) => setState(item)}
         />
